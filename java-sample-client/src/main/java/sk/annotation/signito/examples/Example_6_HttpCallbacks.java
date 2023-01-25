676faa8bee720f260392fb2054fdb170bc403554
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2023-01-25 13:43:36 +0100

    updated httpCallbacks

--- java-sample-client/src/main/java/sk/annotation/signito/examples/Example_6_HttpCallbacks.java
+++ java-sample-client/src/main/java/sk/annotation/signito/examples/Example_6_HttpCallbacks.java
@@ -9,8 +9,11 @@ import sk.annotation.projects.signito.web.HttpResponse;
 import sk.annotation.signito.examples.utils.ExampleUtils;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 
@@ -26,23 +29,27 @@ public class Example_6_HttpCallbacks {
     public Example_6_HttpCallbacks() {
         HttpCallbacks httpCallbacks = new HttpCallbacks() {
             @Override
-            public void beforeRequest(HttpRequest httpRequest) {
-                System.out.println("before request " + httpRequest.getUrl() + " at " + httpRequest.getTimestamp().toString());
+            public void beforeRequest(HttpRequest req) {
+                System.out.println("before request " + req.getRequestMethod() + ": "+ req.getUrl() + " at " + req.getTimestamp() + ", body: " + req.getPostBody());
             }
 
             @Override
-            public void afterResponse(HttpResponse<?> httpResponse, HttpRequest httpRequest) {
-                System.out.println("after response " + httpResponse.getResponseCode() + " at " + httpResponse.getTimestamp().toString());
+            public void afterResponse(HttpResponse<?> httpResponse, HttpRequest resp) {
+                System.out.println("after response " + httpResponse.getResponseCode() + " at " + httpResponse.getTimestamp() + " " + httpResponse.getDataRaw());
             }
 
             @Override
             public void afterFileUploadResponse(HttpResponse<?> httpResponse, HttpRequest httpRequest) {
-
+                //do something
             }
 
             @Override
             public void onConnectionError(HttpRequest httpRequest, IOException e, Instant instant) {
-                System.out.println("Connection error");
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+
+                System.out.println("Connection error: " + sw);
             }
         };
         SignitoClient signitoClient = ExampleUtils.createSignitoClient(httpCallbacks);
@@ -50,7 +57,9 @@ public class Example_6_HttpCallbacks {
         // do some arbitrary call
         DocumentGroupFilterDTO filter = new DocumentGroupFilterDTO();
         filter.setStatus(List.of(DocumentStatusEnum.DONE));
-        filter.setLastModifiedFrom(ZonedDateTime.from(LocalDateTime.now()));
+        LocalDateTime lastChecked = LocalDateTime.of(2023, 1, 1, 12, 0);
+
+        filter.setLastModifiedFrom(lastChecked.atZone(ZoneId.systemDefault()));
         signitoClient.searchDocumentGroups(filter);
     }
 }
